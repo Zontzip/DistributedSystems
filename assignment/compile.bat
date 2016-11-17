09b57425e326d75a9d6e96945e11d64954b0e0ea
Zontzip <zontzor@gmail.com>
2016-11-17 05:01:01 +0000

    Add bat files

---
+++ assignment/compile.bat
@@ -0,0 +1,3 @@
+javac -classpath .\classes; -d .\classes ./src/ie/dit/business/*.java
+javac -classpath .\classes; -d .\classes ./src/ie/dit/client/*.java
+javac -classpath .\classes; -d .\classes ./src/ie/dit/server/*.java
---
+++ assignment/startServer.bat
@@ -0,0 +1,1 @@
+java -cp .\classes ie.dit.server.AuctionServer
